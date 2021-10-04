c1481916b7a5142b4aaab50c1da44ebfabe599a0
McKenna Penley <mckennapenley@gmail.com>
2021-10-03 22:36:01 -0400

    add edit view

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-
 //___________________
 //Dependencies
 //___________________
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config();
-const mongoose = require ('mongoose');
-const session = require('express-session');
-const methodOverride = require('method-override');
-const Video = require('./models/video.js');
-
+require("dotenv").config();
+const mongoose = require("mongoose");
+const session = require("express-session");
+const methodOverride = require("method-override");
+const Video = require("./models/video.js");
 
 //___________________
 //Port
@@ -28,56 +26,50 @@ const DATABASE_URL = process.env.DATABASE_URL;
 // May or may not need these depending on your Mongoose version
 // Database Configuration
 mongoose.connect(process.env.DATABASE_URL, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 // Database Connection Error / Success
 const db = mongoose.connection;
-db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
-db.on('connected', () => console.log('mongo connected'));
-db.on('disconnected', () => console.log('mongo disconnected'));
+db.on("error", (err) => console.log(err.message + " is mongod not running?"));
+db.on("connected", () => console.log("mongo connected"));
+db.on("disconnected", () => console.log("mongo disconnected"));
 
 //___________________
 //Middleware
 //___________________
 
 //use public folder for static assets
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // populates req.body with parsed info from forms - if no data from forms will return an empty object {}
-app.use(express.urlencoded({ extended: true }));// extended: false - does not allow nested objects in query strings
+app.use(express.urlencoded({ extended: true })); // extended: false - does not allow nested objects in query strings
 //app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
 
 //use method override
-app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
+app.use(methodOverride("_method")); // allow POST, PUT and DELETE from a form
 
 //boilerplate for sessions set up
 app.use(
   session({
-      secret: process.env.SECRET,
-      resave: false,
-      saveUninitialized: false
-  }));
-
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 //___________________
 // Routes
 //___________________
 //localhost:3000
 
-
 // Routes / Controllers
-const userController = require('./controllers/users.js');
-app.use('/users', userController);
+const userController = require("./controllers/users.js");
+app.use("/users", userController);
 
-const sessionsController = require('./controllers/sessions.js');
-app.use('/sessions', sessionsController);
-
-
-
-
-
+const sessionsController = require("./controllers/sessions.js");
+app.use("/sessions", sessionsController);
 
 // //index & dashboard view
 // app.get('/', (req, res) => {
@@ -88,65 +80,59 @@ app.use('/sessions', sessionsController);
 // 	} else {
 // 		res.render('index.ejs', {
 // 			currentUser: req.session.currentUser,
-    
+
 // 		});
 // 	}
 // });
 
-
 //index & dashboard view
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
+  const path = req.session.currentUser ? "dashoard.ejs" : "index.ejs";
 
-
-	if (req.session.currentUser) {
-    Video.find({}, (error, allVideos) => {
-      
-		res.render('dashboard.ejs', {
-			currentUser: req.session.currentUser,
-      videos: allVideos
-		})})
-    } else {
-      Video.find({}, (error, allVideos) => {
-        
-        res.render('index.ejs', {
-          currentUser: req.session.currentUser,
-          videos: allVideos
-        })})
-        }
-
+  Video.find({}, (error, allVideos) => {
+    res.render(path, {
+      currentUser: req.session.currentUser,
+      videos: allVideos,
+    });
+  });
 });
 
- // New
- app.get('/new', (req, res) => {
-	res.render('new.ejs');
+// New
+app.get("/new", (req, res) => {
+  res.render("new.ejs");
 });
 
 // Create
-app.post('/', (req, res) => {
-
-	Video.create(req.body, (error, createdVideo) => {
-		console.log(error)
-		res.redirect('/');
-	});
-  console.log(req.body)
-  
+app.post("/", (req, res) => {
+  Video.create(req.body, (error, createdVideo) => {
+    console.log(error);
+    res.redirect("/");
+  });
+  console.log(req.body);
 });
 
+//Edit Route
+app.get("/videos/:idx/edit", (req, res) => {
+  Video.findById(req.params.idx, (err, foundVideo) => {
+    res.render("edit.ejs", {
+      video: foundVideo,
+    });
+  });
+});
 
 // Seed
-const videoSeed = require('./models/videoSeed.js');
+const videoSeed = require("./models/videoSeed.js");
 
-app.get('/seed', (req, res) => {
-  console.log('seed route ran')
+app.get("/seed", (req, res) => {
+  console.log("seed route ran");
   Video.deleteMany({}, (error, allVideos) => {});
 
   Video.create(videoSeed, (error, data) => {
-		res.redirect('/');
-	});
-
+    res.redirect("/");
+  });
 });
 
 //___________________
 //Listener
 //___________________
-app.listen(PORT, () => console.log('express is listening on:', PORT));
+app.listen(PORT, () => console.log("express is listening on:", PORT));
