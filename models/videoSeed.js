33678c1d0c43b469dfa21436470933516863026b
McKenna Penley <mckennapenley@gmail.com>
2021-10-06 23:20:16 -0400

    updates navbar styling, adds more seed data

--- models/videoSeed.js
+++ models/videoSeed.js
@@ -1,6 +1,6 @@
 module.exports = [
   {
-    title: "Scalp Check",
+    title: "Scalp Exam and Treatment",
     url: "https://www.youtube.com/embed/WkvJCGSNtpI",
     tags: ["hair", "doctor"],
     votes: 10,
@@ -11,20 +11,53 @@ module.exports = [
     ],
   },
   {
-    title: "Make Up",
+    title: "ASMR Make Up Spa",
     url: "https://www.youtube.com/embed/AlK1oRLr-Cc",
     tags: "make-up",
     votes: 20,
     comments: [{ user: "mckenna@email.com", comment: "Best video ever" }],
   },
   {
-    title: "Fabrics",
+    title: "Fancy Fabrics",
     url: "https://www.youtube.com/embed/599LMnGLC9k",
     tags: "brushing",
     votes: 15,
     comments: [
       { user: "mckenna@email.com", comment: "So good!" },
       { user: "jeryy@email.com", comment: "Hate this one" },
+    ],
+  },
+  {
+    title: "ASMR Soap Carving",
+    url: "https://www.youtube.com/embed/orD6yIilR0A",
+    tags: ["soap carving"],
+    votes: 30,
+    comments: [
+      { user: "ben@email.com", comment: "Wow I love soap" },
+      { user: "george@email.com", comment: "This is cool!" },
+      { user: "sally@email.com", comment: "Don't like this one!!" },
+    ],
+  },
+  {
+    title: "Pineapple Relaxation",
+    url: "https://www.youtube.com/embed/V_A9sAaNawU",
+    tags: ["weird, pineapple"],
+    votes: -5,
+    comments: [
+      { user: "jerry@email.com", comment: "Do not like" },
+      { user: "elaine@email.com", comment: "absolutely hate it" },
+      { user: "kramer@email.com", comment: "Wouldn't watch again" },
+    ],
+  },
+  {
+    title: "Unboxing Windchimes",
+    url: "https://www.youtube.com/embed/4FsLc81iRGo",
+    tags: ["unboxing"],
+    votes: 25,
+    comments: [
+      { user: "jerry@email.com", comment: "Huge fan" },
+      { user: "elaine@email.com", comment: "Would watch this one everyday" },
+      { user: "kramer@email.com", comment: "I like it!" },
     ],
   },
 ];
